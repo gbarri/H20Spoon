68d4d308c6dea04ca27e9b97a203d3f0b8707684
Barri Giacomo <g.barri@reply.it>
2021-02-19 01:14:47 +0100

    rimozione not used sections

--- src/main/java/H2OSpoon/ModelFeatures.java
+++ src/main/java/H2OSpoon/ModelFeatures.java
@@ -6,10 +6,8 @@ import java.util.List;
 public class ModelFeatures {
     private String modelName;
     private List<String> fieldInputNames = new ArrayList<>();
-    private List<String> getFieldOutputNames = new ArrayList<>();
 
-/*    FARE la lettura da un csv!
-    esercizio su intepretare la predizione
+/*  esercizio su intepretare la predizione
    grafici in funzione di una delle 5 variabili??
 */
 
@@ -32,11 +30,4 @@ public class ModelFeatures {
         this.fieldInputNames = fieldInputNames;
     }
 
-    public List<String> getGetFieldOutputNames() {
-        return getFieldOutputNames;
-    }
-
-    public void setGetFieldOutputNames(List<String> getFieldOutputNames) {
-        this.getFieldOutputNames = getFieldOutputNames;
-    }
 }
