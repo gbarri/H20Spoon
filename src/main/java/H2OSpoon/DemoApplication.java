3e8e210312ecfbca14378e938f59bf8a083bb5db
Barri Giacomo <g.barri@reply.it>
2021-03-02 00:04:24 +0100

    cleaning imports

--- src/main/java/H2OSpoon/DemoApplication.java
+++ src/main/java/H2OSpoon/DemoApplication.java
@@ -1,16 +1,7 @@
 package H2OSpoon;
 
-import hex.genmodel.easy.EasyPredictModelWrapper;
-import hex.genmodel.easy.RowData;
-import hex.genmodel.easy.prediction.BinomialModelPrediction;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
-import org.springframework.core.type.filter.AssignableTypeFilter;
 
 @SpringBootApplication
 public class DemoApplication {
